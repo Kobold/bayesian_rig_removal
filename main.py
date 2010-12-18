e3d22653eed16290cc7db4093c4a501ec24da670
Andy Kish <agkish@gmail.com>
2010-12-17 23:04:54 -0500

    cleanup

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 from scipy import ndimage
 from numpy import linalg
 from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import re
+import time
 
 SIGMA_E = 0.01 # allowance of acceleration
 SIGMA_V = 0.01 # allowance of acceleration
@@ -13,35 +13,9 @@ LAMBDA = 2.0
 ALPHA = 0.0331 # ~ 3 * SIGMA_V according to section 3.4
 
 
-def rig_matte((height, width), vectors, dtype=np.float_):
-    """
-    height, width - dimensions of the output matrix
-    vectors - a list of (x, y) tuple vectors that specify the vertices of a polygonal rig region
-    """
-    img = Image.new('L', (width, height), 1)
-    ImageDraw.Draw(img).polygon(vectors, outline=0, fill=0)
-    return np.array(img, dtype=dtype)
-
-def norm(vectors):
-    return vectors[:,:,0] ** 2. + vectors[:,:,1] ** 2.
-
-def load_d(prefix):
-    """Loads the displacement matrix from csv files. Returns a height x width x 2 matrix."""
-    vel_x = np.genfromtxt(file('%s_x.csv' % prefix), delimiter=',')
-    vel_y = np.genfromtxt(file('%s_y.csv' % prefix), delimiter=',')
-
-    # make a 3d height x width x 2 matrix to hold the vectors
-    vel = np.zeros(list(vel_x.shape) + [2])
-    vel[:, :, 0] = vel_y # note, this y here is correct--and it's important it be this order
-    vel[:, :, 1] = vel_x
-    return vel
-
-def vector_weighted_average(vf, weights):
-    """Returns the average vector of vector field ``vf``."""
-    weights_sum = weights.sum()
-    y_average = (vf[:,:,0] * weights).sum() / weights_sum
-    x_average = (vf[:,:,1] * weights).sum() / weights_sum
-    return np.array([y_average, x_average])
+#
+# Helpers
+#
 
 def bounding_box(vertices, (height, width), extend=5):
     """Returns the bounding box of ``vertices`` plus some boundary ``extend``.
@@ -55,11 +29,79 @@ def bounding_box(vertices, (height, width), extend=5):
     
     return max(x_min, 0), min(x_max, width), max(y_min, 0), min(y_max, height)
 
+def frame_string(path):
+    """Extracts a frame string like '004' from a path like 'Forest_Gump/004.png'."""
+    filename = os.path.split(path)[1]
+    return os.path.splitext(filename)[0]
+
+def index_iterator((x_min, x_max, y_min, y_max)):
+    """Yieds indices within a given range. Makes it easy to iterate over subsections of
+    a matrix."""
+    for row in xrange(y_min, y_max):
+        for col in xrange(x_min, x_max):
+            yield (row, col)
+
+def load_d(prefix):
+    """Loads optical flow displacement from csv. Returns a height x width x 2 matrix.
+    
+    prefix - the common prefix of the x and y optical flow files
+    """
+    vel_x = np.genfromtxt(file('%s_x.csv' % prefix), delimiter=',')
+    vel_y = np.genfromtxt(file('%s_y.csv' % prefix), delimiter=',')
+
+    # make a 3d height x width x 2 matrix to hold the vectors
+    vel = np.zeros(list(vel_x.shape) + [2])
+    vel[:, :, 0] = vel_y # note, this y here is correct--and it's important it be this order
+    vel[:, :, 1] = vel_x
+    return vel
+
+def parse_rig_vertices(f):
+    """Given the file ``f``, parses the vertices of the rig for each frame out of it."""
+    vertices_list = []
+    for line in f:
+        vertices = line.split()
+        point = [tuple(map(int, str_point.split(','))) for str_point in vertices]
+        vertices_list.append(point)
+
+    return vertices_list
+
+def rig_matte((height, width), vectors, dtype=np.float_):
+    """Returns a matte with a polygonal hole punched out.
+    
+    height, width - dimensions of the output matrix
+    vectors - a list of (x, y) tuple vectors that specify the vertices of a polygonal rig region
+    dtype - the type of matrix to return
+    """
+    img = Image.new('L', (width, height), 1)
+    ImageDraw.Draw(img).polygon(vectors, outline=0, fill=0)
+    return np.array(img, dtype=dtype)
+
+def save_image(filename, matrix):
+    """Saves ``matrix`` to a black and white image named ``filename``."""
+    im = Image.new('L', list(reversed(matrix.shape)))
+    data = np.floor(np.ravel(matrix) * 256)
+    im.putdata(data)
+    im.save(filename)
+
+def vector_weighted_average(vf, weights):
+    """Returns the average vector of vector field ``vf`` weighted by ``weights``."""
+    weights_sum = weights.sum()
+    y_average = (vf[:,:,0] * weights).sum() / weights_sum
+    x_average = (vf[:,:,1] * weights).sum() / weights_sum
+    return np.array([y_average, x_average])
+
+#
+# Interpolation for Candidate Motion Vectors
+#
+
 def spatial_interpolation_vector(d, rig_vertices):
     """Returns the vector for all the spatially interpolated candidates. (section 4.2)
     
-    Note that this uses the second, ghetto, method of assuming everything behind the rig moves with
-    one motion.
+    Note that this uses the second, more simple, method of assuming everything behind the rig
+    moves with one motion.
+    
+    d - a displacement matrix
+    rig_vertices - the vertices of an area to not include in interpolation
     """
     shape = d.shape[:2]
     x_min, x_max, y_min, y_max = bounding_box(rig_vertices, shape)
@@ -68,6 +110,12 @@ def spatial_interpolation_vector(d, rig_vertices):
                                    matte[y_min:y_max, x_min:x_max])
 
 def temporal_interpolation_vectors(d_prev, candidates, bounds):
+    """Interpolates motion candidates from previous frames. (section 4.3)
+    
+    d_prev - d_n-1,n-2 from the paper. Motion from frame n-1 to n-2.
+    candidates - a matrix of lists--interpolated candidates will be added to the appropriate location
+    bounds - a bounding box surrounding the rig area
+    """
     assert d_prev.shape[:2] == candidates.shape
     
     rows, cols = candidates.shape
@@ -77,7 +125,7 @@ def temporal_interpolation_vectors(d_prev, candidates, bounds):
             candidates[c_row, c_col].append(d_prev[index])
 
 #
-# Energy calculations
+# Motion energy components
 #
 
 def E_l(x_r, x_r_prime, w_n, w_n_1, I_n, I_n_1):
@@ -85,6 +133,8 @@ def E_l(x_r, x_r_prime, w_n, w_n_1, I_n, I_n_1):
     
     x_r
         a rig site
+    x_r_prime
+        rig site in next frame based off candidate interpolation
     w_n
         continuous matrix. 1 indicates data available, 0 indicates data missing.
         in the rig area w(x_r) = 0, i.e. this is the "not-rig" matrix
@@ -108,14 +158,13 @@ def E_0_t(x_r, x_r_prime, w_n_1, candidate, d_prev):
 
     x_r:
         a rig site
-    occlusion: o_n,n-1
-        binary matrix. 1 indicates data that that point in the frame n does not
-        exist in the frame n-1. 0 indicates no discontinuity
+    x_r_prime
+        rig site in next frame based off candidate interpolation
     w_n_1: w_n-1
         continuous matrix. 1 indicates data available, 0 indicates data missing.
         in the rig area w(x_r) = 0, i.e. this is the "not-rig" matrix
-    d_h: d^h_n,n-1
-        vector matrix estimating the motion of the hidden area
+    candidate: d^h_n,n-1
+        vector estimating the motion of the hidden area
     d_prev: d_n-1,n-2
         vector matrix with the motion mapping from frame n-1 to frame n-2
     """
@@ -126,12 +175,15 @@ def E_0_t(x_r, x_r_prime, w_n_1, candidate, d_prev):
     return temp
 
 def neighborhood((y, x), (height, width)):
+    """Returns coordinates in the neighborhood of (y, x) omitting coordinates that go outside
+    the bounds of (height, width)."""
     return [(yt, xt) for xt in [x + 1, x, x - 1]
                      for yt in [y + 1, y, y - 1]
                      if 0 <= xt < width and 0 <= yt < height
                      and (xt, yt) != (x, y)]
 
 def lambda_(s, x_r):
+    """Weights against points farther away from x_r. As defined in section 3 of the paper."""
     return LAMBDA / linalg.norm(np.array(s) - np.array(x_r))
 
 def E_s(x_r, candidate, d_h):
@@ -142,14 +194,14 @@ def E_s(x_r, candidate, d_h):
     return temp
 
 def E_0_o(x_r, occlusion):
-    """E_0_o - Spatial occlusion smoothness"""
+    """E_0_o - Spatial occlusion smoothness (equation 9)"""
     temp = sum(lambda_(s, x_r) * abs(occlusion[s]) ** 2.
                for s in neighborhood(x_r, occlusion.shape[:2]))
 
     return temp
 
 def E_1_o(x_r, occlusion):
-    """E_1_o - Spatial occlusion smoothness"""
+    """E_1_o - Spatial occlusion smoothness (equation 9)"""
     temp = sum(lambda_(s, x_r) * abs(1. - occlusion[s]) ** 2.
                for s in neighborhood(x_r, occlusion.shape[:2]))
 
@@ -157,41 +209,21 @@ def E_1_o(x_r, occlusion):
 
 
 #
-# Tools
+# Main 
 #
 
-def vector_display(vf):
-    """Draws the vector field magnitudes of ``vf``."""
-    vf_mag = np.sqrt(norm(vf))
-    plt.imshow(vf_mag / vf_mag.max(), cmap='gray', interpolation='nearest')
-    plt.show()
-
-def save_image(filename, matrix):
-    im = Image.new('L', list(reversed(matrix.shape)))
-    data = np.floor(np.ravel(matrix) * 256)
-    im.putdata(data)
-    im.save(filename)
-
-def parse_rig_vertices(f):
-    vertices_list = []
-    for line in f:
-        vertices = line.split()
-        point = [tuple(map(int, str_point.split(','))) for str_point in vertices]
-        vertices_list.append(point)
-    
-    return vertices_list
-
-def frame_string(path):
-    """Extracts a frame string like '004' from a path like 'Forest_Gump/004.png'."""
-    filename = os.path.split(path)[1]
-    return os.path.splitext(filename)[0]
-
-def index_iterator((x_min, x_max, y_min, y_max)):
-    for row in xrange(y_min, y_max):
-        for col in xrange(x_min, x_max):
-            yield (row, col)
-
 def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
+    """Reconstructs the given frame I_n, returning the new version along with updated weights.
+    
+    displacement - d_n,n-1, optical flow for the non rig areas of the current frame
+    d_prev - d_n-1,n-2, optical flow mapping from the previous frame to the frame before that.
+        Used for interpolation.
+    vertices - vertices of the polygon outlining the rig region
+    w_n - the weights for the current frame
+    w_n_1 - w_n-1, the weights for the previous frame
+    I_n - image intensity for the current frame
+    I_n_1 - I_n-1, image intensity for the previous frame
+    """
     shape = I_n.shape
     bounds = bounding_box(vertices, shape)
     
@@ -199,33 +231,30 @@ def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
     siv = spatial_interpolation_vector(displacement, vertices)
     
     # initialize the candidates for the motion with the spatial interpolation
-    print 'initializing candidates'
     candidates = np.empty(shape, dtype=object)
     for index, y in np.ndenumerate(candidates):
         candidates[index] = [siv]
-    print 'candidate # =', sum(len(x) for x in candidates.flat)
     
     # find temporal interpolation candidates (section 4.3)
     temporal_interpolation_vectors(d_prev, candidates, bounds)
-    print 'candidate # =', sum(len(x) for x in candidates.flat)
     
     # add adjacent neighbors as candidates if they've been assigned
-    print 'adding additional candidates'
     for x_r in index_iterator(bounds):
         if w_n[x_r] == 1:
             candidate = displacement[x_r]
             for s in neighborhood(x_r, shape):
                 if w_n[s] < 1:
                     candidates[s].append(candidate)
-    print 'candidate # =', sum(len(x) for x in candidates.flat)
     
     # candidate evaluation (section 4.4)
+    # these are the initial candidates for occlusion and the hidden displacement
     occluded = np.logical_not(rig_matte(shape, vertices, dtype=bool))
     perturb = np.random.randn(*d_prev.shape) / 6.
     d_h = np.where(np.dstack((occluded, occluded)),
-                   np.tile(siv, list(shape) + [1]) + perturb, # initialize with spatial interp
+                   np.tile(siv, list(shape) + [1]) + perturb,
                    displacement)
     
+    # the updated occlusion and displacement values will be placed in these
     new_occluded = occluded.copy()
     new_d_h = d_h.copy()
     is_rig = occluded.copy()
@@ -234,7 +263,8 @@ def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
             minimum_energy = float('inf')
             best_candidate = None # the candidate associated with the minimum energy
             best_is_occluded = None
-        
+            
+            # find the candidate that minimizes the local energy
             for candidate in candidates[x_r]:
                 # the motion compensated site x_r' = x_r + d^h_n,n-1(x_r)
                 x_r_prime = tuple((np.array(x_r) + candidate).round())
@@ -249,6 +279,7 @@ def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
                 e0 = el + e0t + es + e0o
                 e1 = el + e1t + es + e1o
             
+                # energy components determine whether the new displacement is occluded or not
                 if e0 < minimum_energy:
                     minimum_energy = e0
                     best_candidate = candidate
@@ -267,7 +298,8 @@ def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
     occluded = new_occluded
     d_h = new_d_h
     
-    # reconstruct that shizzle
+    # construct the new intensities and weights based upon the computed motion vectors
+    # using linear interpolation
     I_h = I_n.copy()
     for x_r in index_iterator(bounds):
         if is_rig[x_r]:
@@ -284,6 +316,8 @@ def reconstruct_frame(displacement, d_prev, vertices, w_n, w_n_1, I_n, I_n_1):
     
 
 if __name__ == '__main__':
+    # This is the main function driving everything.
+    
     # load the images
     print 'loading images'
     load = lambda fname: ndimage.imread(fname, flatten=True) / 255.
@@ -305,10 +339,13 @@ if __name__ == '__main__':
                              for f in os.listdir(displacement_dir) if f.endswith('.csv'))
     displacements = [load_d(os.path.join(displacement_dir, dn))
                      for dn in sorted(displacement_names)]
-    
+
+    # loop over the images progressively reconstructing them
     print 'reconstructing frames'
     for i in xrange(len(images)-2):
         print '\nreconstructing frame', i
+        
+        # reconstruct the frame
         im, w_h = reconstruct_frame(
             displacements[i+1],
             displacements[i],
@@ -317,8 +354,8 @@ if __name__ == '__main__':
             weights[i+1],
             images[i+2],
             images[i+1])
-        save_image('%03d.png' % (i + 2), im)
         
+        # save out an image and update the stored image intensities and weights
+        save_image('out/%03d.png' % (i + 2), im)
         images[i+2] = im
         weights[i+2] = w_h
-    
